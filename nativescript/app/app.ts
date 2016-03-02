62456d695913120b84678a20538b4d50e2720dd9
Nathan Walker <walkerrunpdx@gmail.com>
2016-03-01 20:43:09 -0800

    small reorg

--- nativescript/app/app.ts
+++ nativescript/app/app.ts
@@ -5,8 +5,11 @@ import 'rxjs/add/operator/map';
 import {nativeScriptBootstrap} from 'nativescript-angular/application';
 import {NS_ROUTER_PROVIDERS, NS_ROUTER_DIRECTIVES} from 'nativescript-angular/router';
 
+// angular 
+import {provide} from 'angular2/core';
+
 // config
-import {CoreConfigService} from './frameworks/core.framework/index';
+import {CoreConfigService, WindowService} from './frameworks/core.framework/index';
 CoreConfigService.PLATFORM_TARGET = CoreConfigService.PLATFORMS.MOBILE_NATIVE;
 CoreConfigService.DEBUG.LEVEL_4 = true;
 CoreConfigService.ROUTER_DIRECTIVES = NS_ROUTER_DIRECTIVES;
@@ -14,8 +17,10 @@ CoreConfigService.ROUTER_DIRECTIVES = NS_ROUTER_DIRECTIVES;
 // app
 import {NS_APP_PROVIDERS} from './frameworks/nativescript.framework/index';
 import {NSAppComponent} from './pages/app/app.component';
+import {WindowNative} from './shared/services/window-native.service';
   
 nativeScriptBootstrap(NSAppComponent, [
+  provide(WindowService, { useClass: WindowNative }),
   NS_ROUTER_PROVIDERS,
   NS_APP_PROVIDERS
 ]);
