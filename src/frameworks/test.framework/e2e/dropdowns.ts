711346e29304bb914bc2ed80cf97fca64b5c7969
Nathan Walker <walkerrunpdx@gmail.com>
2016-03-20 10:32:04 -0700

    fix(e2e): updated protractor 3.2.1 and fixed integration tests
    closes https://github.com/NathanWalker/angular2-seed-advanced/issues/12

--- src/frameworks/test.framework/e2e/dropdowns.ts
+++ src/frameworks/test.framework/e2e/dropdowns.ts
@@ -1,10 +1,10 @@
 /**
-* Usage: selectDropdownByNumber ( element, index)
-* element : select element
+* Usage: selectDropdownByNumber ( selector, index)
+* selector : select element
 * index : index in the dropdown, 1 base.
 */
-export function selectDropdownByNumber(element: any, index: number, milliseconds: number) {
-  element.findElements(by.tagName('option'))
+export function selectDropdownByNumber(selector: string, index: number, milliseconds: number) {
+  element(by.css(selector)).all(by.tagName('option'))
     .then(function(options: any) {
       options[index].click();
     });
@@ -15,40 +15,25 @@ export function selectDropdownByNumber(element: any, index: number, milliseconds
 
 
 /**
-* Usage: selectDropdownByText (selector, item)
+* Usage: selectDropdownByValue (selector, item)
 * selector : select element
 * item : option(s) in the dropdown.
 */
-export function selectDropdownByText(element: any, item: string, milliseconds: number) {
-  var desiredOption: any;
-  element.findElements(by.tagName('option'))
-    .then(function findMatchingOption(options: any) {
-      options.some(function(option: any) {
-        option.getText().then(function doesOptionMatch(text: string) {
-          if (text.indexOf(item) !== -1) {
-            desiredOption = option;
-            return true;
-          }
-        });
-      });
-    })
-    .then(function clickOption() {
-      if (desiredOption) {
-        desiredOption.click();
-      }
-    });
+export function selectDropdownByValue(selector: string, item: string, milliseconds: number) {
+  // var desiredOption: any;
+  element(by.css(selector)).sendKeys(item);
   if (typeof milliseconds !== 'undefined') {
     browser.sleep(milliseconds);
   }
 }
 
 /**
-* Usage: selectRandomDropdownReturnText ( element, milliseconds)
-* element : select random element
+* Usage: selectRandomDropdownReturnText ( selector, milliseconds)
+* selector : select random element
 * index : wait time to select value for drop down.
 */
-export function selectRandomDropdownReturnText(element: any, milliseconds: number) {
-  element.findElements(by.tagName('option')).then(function(options: any) {
+export function selectRandomDropdownReturnText(selector: string, milliseconds: number) {
+  element(by.css(selector)).all(by.tagName('option')).then(function(options: any) {
     var randomNumber = Math.floor((Math.random() * options.length
     ));
     options[randomNumber].click();
