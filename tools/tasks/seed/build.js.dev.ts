7de474118ed163898950edfac7c032700a52b61c
jvitor83 <jvitor83@hotmail.com>
2016-07-11 19:03:44 -0400

    Update build.js.dev.ts
    Fix for chrome sourcemap.

--- tools/tasks/seed/build.js.dev.ts
+++ tools/tasks/seed/build.js.dev.ts
@@ -31,7 +31,7 @@ export = () => {
     .pipe(plugins.typescript(tsProject));
 
   return result.js
-    .pipe(plugins.sourcemaps.write('.', {includeContent: false, sourceRoot: '../../src/client'}))
+    .pipe(plugins.sourcemaps.write('.', {includeContent: true, sourceRoot: '../../src/client'}))
     .pipe(plugins.template(templateLocals()))
     .pipe(gulp.dest(APP_DEST));
 };
