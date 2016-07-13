fe43a59425a5f1559f3e1aabb5ffd14d10a99793
Morten Sjøgren <m_abs@mabs.dk>
2016-07-13 20:49:11 +0200

    Component CSS for {N} is now moved to tns.css-files

--- src/client/app/frameworks/core/decorators/utils.ts
+++ src/client/app/frameworks/core/decorators/utils.ts
@@ -20,45 +20,50 @@ export class DecoratorUtils {
     // default directives
     let DIRECTIVES: any[] = [];
     // default pipes
-    let PIPES: any[] = [TranslatePipe];   
-    
+    let PIPES: any[] = [TranslatePipe];
+
     // custom decorator options
     if (customDecoratorMetadata) {
       if (customDecoratorMetadata.directives) {
-        DIRECTIVES.push(...customDecoratorMetadata.directives); 
+        DIRECTIVES.push(...customDecoratorMetadata.directives);
       }
       if (customDecoratorMetadata.pipes) {
-        PIPES.push(...customDecoratorMetadata.pipes); 
+        PIPES.push(...customDecoratorMetadata.pipes);
       }
     }
-    
+
     if (metadata.templateUrl) {
       // correct view for platform target
       metadata.templateUrl = ViewBrokerService.TEMPLATE_URL(metadata.templateUrl);
     }
-    
+
+    if (metadata.styleUrls) {
+      // correct view for platform target
+      metadata.styleUrls = ViewBrokerService.STYLE_URLS(metadata.styleUrls);
+    }
+
     metadata.directives = metadata.directives ? metadata.directives.concat(DIRECTIVES) : DIRECTIVES;
     metadata.pipes = metadata.pipes ? metadata.pipes.concat(PIPES) : PIPES;
-    
+
     if (metadata.changeDetection) {
       metadata.changeDetection = metadata.changeDetection;
     } else {
       // default OnPush
       metadata.changeDetection = ChangeDetectionStrategy.OnPush;
     }
-    
+
     if (metadata.encapsulation) {
       metadata.encapsulation = metadata.encapsulation;
     }
-    
-    // initialize anything 
+
+    // initialize anything
     if (metadata.init) {
       metadata.init();
-    }   
+    }
 
     return metadata;
   }
-  
+
   public static annotateComponent(cls: any, metadata: any = {}, customDecoratorMetadata?: any) {
     let annotations = _reflect.getMetadata('annotations', cls) || [];
     annotations.push(new Component(DecoratorUtils.getMetadata(metadata, customDecoratorMetadata)));
