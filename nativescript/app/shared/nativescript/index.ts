0d1ffa17da2c6fd7fbc6bb57b71eba6fcd8781a2
Iván González <ivannugo@gmail.com>
2016-07-20 20:13:07 +0200

    HTTP error in NativeScript (#171)
    Fixed with @NathanWalker pull: https://github.com/NativeScript/nativescript-angular/pull/321

--- nativescript/app/shared/nativescript/index.ts
+++ nativescript/app/shared/nativescript/index.ts
@@ -1,12 +1,12 @@
 // angular
 import {provide} from '@angular/core';
-import {HTTP_PROVIDERS} from '@angular/http';
 
 // libs
 import {Angulartics2} from 'angulartics2';
 import {Angulartics2Segment} from 'angulartics2/src/providers/angulartics2-segment';
 import {provideStore} from '@ngrx/store';
 import {TranslateService} from 'ng2-translate/ng2-translate';
+import {NS_HTTP_PROVIDERS} from 'nativescript-angular/http';
 
 // app
 import {ConsoleService, LogService} from '../../app/frameworks/core/index';
@@ -18,7 +18,7 @@ import {NSAngulartics2Segment} from './services/ns-angulartics2-segment.service'
 MultilingualService.SUPPORTED_LANGUAGES = AppConfigService.SUPPORTED_LANGUAGES;
 
 export const NS_APP_PROVIDERS: any[] = [
-  HTTP_PROVIDERS,
+  NS_HTTP_PROVIDERS,
   provide(ConsoleService, { useValue: console }),
   LogService,
   provideStore({  
