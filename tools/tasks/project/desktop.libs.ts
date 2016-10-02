e86c8c9d68fd096b831de73487d129c0c550a1e0
dcsw <dcsw>
2016-10-01 22:18:57 -0400

    Speed up compiles with selective use of gulp-newer to only compile changed files.

--- tools/tasks/project/desktop.libs.ts
+++ tools/tasks/project/desktop.libs.ts
@@ -1,6 +1,7 @@
 import * as gulp from 'gulp';
 import { relative, join } from 'path';
 import Config from '../../config';
+var newer = require('gulp-newer');
 
 export = () => {
   let src = [
@@ -15,5 +16,9 @@ export = () => {
   src.push(...Config.NPM_DEPENDENCIES.map(x => relative(Config.PROJECT_ROOT, x.src)));
 
   return gulp.src(src, { base: 'node_modules' })
+    .pipe(newer({
+      dest: join(Config.APP_DEST + '/node_modules'), 
+      map: function(path: String) { return path.replace('.ts', '.js').replace('.sccs', '.css'); }
+    }))
     .pipe(gulp.dest(join(Config.APP_DEST + '/node_modules')));
 };
