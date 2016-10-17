de74af5371fdb008f17da93fe87b9e683b859dc7
Nathan Walker <walkerrunpdx@gmail.com>
2016-10-16 21:25:43 -0700

    fix(tests): e2e/integration

--- protractor.conf.js
+++ protractor.conf.js
@@ -28,8 +28,14 @@ const config = {
 
   onPrepare: function() {
     // needed for custom class decorators
-    require("reflect-metadata");
+    // require("reflect-metadata");
     require("zone.js/dist/zone-node");
+    require("zone.js/dist/long-stack-trace-zone");
+    require("zone.js/dist/async-test");
+    require("zone.js/dist/fake-async-test");
+    require("zone.js/dist/sync-test");
+    require("zone.js/dist/proxy");
+    require("zone.js/dist/jasmine-patch");
     browser.ignoreSynchronization = false;
   },
 
