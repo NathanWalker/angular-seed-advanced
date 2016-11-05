84852f158a7bdb5d53b581d6cd9d6ccd393d406b
ianm <ianm@completesystemsupport.com>
2016-11-04 20:17:38 -0400

    prod mode fix for analytics, and project override

--- tools/config/seed-advanced.config.ts
+++ tools/config/seed-advanced.config.ts
@@ -72,7 +72,6 @@ export class SeedAdvancedConfig extends SeedConfig {
     this.SYSTEM_CONFIG.paths[this.BOOTSTRAP_MODULE] = `${this.APP_BASE}${this.BOOTSTRAP_MODULE}`;
     this.SYSTEM_CONFIG.paths['angulartics2'] = `${this.APP_BASE}node_modules/angulartics2/dist/index`;
     this.SYSTEM_CONFIG.paths['angulartics2/*'] = `${this.APP_BASE}node_modules/angulartics2/dist/*`;
-    //this.SYSTEM_CONFIG.paths['angulartics2/*'] = `${this.APP_BASE}node_modules/angulartics2/dist/*`;
     this.SYSTEM_CONFIG.paths['lodash'] = `${this.APP_BASE}node_modules/lodash/index`;
 
     // testing support for @ngrx/effects
@@ -97,7 +96,13 @@ export class SeedAdvancedConfig extends SeedConfig {
       main: 'bundles/index.js',
       defaultExtension: 'js'
     };
+    this.SYSTEM_CONFIG['packages']['angulartics2/providers'] = {
+      main: 'index.js',
+      defaultExtension: 'js'
+    };
+    
     this.SYSTEM_BUILDER_CONFIG.paths['angulartics2'] = `node_modules/angulartics2/dist/index.js`;
+    this.SYSTEM_BUILDER_CONFIG.paths['angulartics2/*'] = `node_modules/angulartics2/dist/providers/`+ this.APP_ANALYTICS + `/angulartics2-`+ this.APP_ANALYTICS + `.js`;
     this.SYSTEM_BUILDER_CONFIG.paths['lodash'] = `node_modules/lodash/index.js`;
     this.SYSTEM_BUILDER_CONFIG.paths['@ngrx/core'] = `node_modules/@ngrx/core/index.js`;
     this.SYSTEM_BUILDER_CONFIG.paths['@ngrx/store'] = `node_modules/@ngrx/store/index.js`;
