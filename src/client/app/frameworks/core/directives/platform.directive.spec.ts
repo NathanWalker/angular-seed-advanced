044fc8c1ed29b5963706ee864097b56eb4e6ae67
Burak Tasci <me@fulls1z3.com>
2016-12-05 14:36:38 +0200

    Update platform.directive.spec.ts
    Updated comments ("// module" for module deps).

--- src/client/app/frameworks/core/directives/platform.directive.spec.ts
+++ src/client/app/frameworks/core/directives/platform.directive.spec.ts
@@ -3,8 +3,10 @@ import { TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
 
 // app
-import { PlatformDirective } from './platform.directive';
 import { t } from '../../test/index';
+
+// module
+import { PlatformDirective } from './platform.directive';
 import { WindowService } from '../../core/index';
 import { WindowMock } from '../../core/testing/index';
 
