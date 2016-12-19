2ada40ae52a362d096387d094c1ecce4952de1de
Nathan Walker <walkerrunpdx@gmail.com>
2016-12-18 19:03:18 -0800

    feat(NativeScript): AoT webpack bundling wip

--- nativescript/src/app.aot.ts
+++ nativescript/src/app.aot.ts
@@ -1,6 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { platformNativeScript } from "nativescript-angular/platform-static";
+import { platformNativeScript } from 'nativescript-angular/platform-static';
 
-import { NativeModuleNgFactory } from "./native.module.ngfactory";
+import { NativeModuleNgFactory } from './native.module.ngfactory';
 
 platformNativeScript().bootstrapModuleFactory(NativeModuleNgFactory);
