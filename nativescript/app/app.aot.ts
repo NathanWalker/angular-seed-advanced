fa4a7add4c68db10bd4f2d9bce6b5bbd6ef00fe5
Nathan Walker <walkerrunpdx@gmail.com>
2017-01-04 20:39:27 -0800

    fix: update deps
    https://github.com/NathanWalker/angular-seed-advanced/issues/327

--- nativescript/app/app.aot.ts
+++
@@ -1,9 +0,0 @@
-// this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { platformNativeScript } from 'nativescript-angular/platform-static';
-
-import { NativeModuleNgFactory } from "./native.module.ngfactory";
-import { enableProdMode } from '@angular/core';
-
-enableProdMode();
-
-platformNativeScript().bootstrapModuleFactory(NativeModuleNgFactory);
--- nativescript/app/vendor-platform.js.ios
+++
@@ -1,3 +0,0 @@
-
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJ2ZW5kb3ItcGxhdGZvcm0uaW9zLmpzIiwic291cmNlc0NvbnRlbnQiOltdfQ==
