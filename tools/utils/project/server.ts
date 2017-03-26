3438d3b0362d5aa1fe14b41717df89ffa159964e
Yavin Five <yavinfive464@gmail.com>
2017-03-27 00:16:35 +0200

    Refactored the server startup code: - Improved the order that methods are called during server startup to match the order in which Express expects things to be configured. - Improved importing and usage of types from Express. - Added some additional exports, as they are likely needed.

--- tools/utils/project/server.ts
+++ tools/utils/project/server.ts
@@ -1,12 +1,26 @@
 import * as express from 'express';
+import { Application } from 'express';
+import * as http from 'http';
 import { SeedWebServer } from '../seed/server';
 
-class ProjectWebServer extends SeedWebServer {
-  // Override and modify the server code here.
+
+export class ProjectWebServer extends SeedWebServer {
+  // Override and modify the server code in this class.
+
+  /**
+   * This method is called once regardless of how the server is started, and this
+   * is the starting point of the project's custom server code.
+   */
+  public startServer() {
+    super.startServer();
+
+    // Add project server startup code here.
+  }
 }
 
-let app: express.Express = express();
-export let webServer: ProjectWebServer = new ProjectWebServer(app);
+let app: Application = express();
+let webServer: ProjectWebServer = new ProjectWebServer(app);
+export default webServer;
 export function serveSPA() { webServer.serveSPA(); }
 export function notifyLiveReload(e: any) { webServer.notifyLiveReload(e); }
 export function serveDocs() { webServer.serveDocs(); }
