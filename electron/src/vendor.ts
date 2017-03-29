90094ff97f55c6c6fcaa0c36281c936ff03fb9dc
ianm <ianm@completesystemsupport.com>
2017-03-28 22:55:56 -0400

    webpack polyfills and vendors

---
+++ electron/src/vendor.ts
@@ -0,0 +1,10 @@
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/core';
+import '@angular/router';
+import '@angular/http';
+
+// RxJS
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/mergeMap';
